4d07b7444a54ccbf4a3c86b6cce56b8f2b05a4b8
Rudolf Tammekivi <rtammekivi@gmail.com>
2014-06-29 02:19:52 +0300

    lk: Import fat_io.
    For devices with VFAT partitions.

---
+++ lib/fat_io/rules.mk
@@ -0,0 +1,13 @@
+LOCAL_DIR := $(GET_LOCAL_DIR)
+
+MODULES +=
+
+OBJS += \
+	$(LOCAL_DIR)/fat_access.o \
+	$(LOCAL_DIR)/fat_cache.o \
+	$(LOCAL_DIR)/fat_filelib.o \
+	$(LOCAL_DIR)/fat_format.o \
+	$(LOCAL_DIR)/fat_misc.o \
+	$(LOCAL_DIR)/fat_string.o \
+	$(LOCAL_DIR)/fat_table.o \
+	$(LOCAL_DIR)/fat_write.o
